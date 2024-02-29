d2681185140c54ae75e09af40ce44252153878a5
tftprodigy <Olurombisammy@gmail.com>
2024-02-29 15:58:16 +0100

    up to variables & strings

---
+++ 02. Variables in python/app.py
@@ -0,0 +1,12 @@
+from typing import Concatenate
+
+
+name = 'Tim'
+age = 18
+print(name)
+print ("My name is",name, "my age is ",age)
+print(name, "is a boy")
+
+# String Concatenation
+print(name + ' is 18')
+print (name + ' is from Niger')
