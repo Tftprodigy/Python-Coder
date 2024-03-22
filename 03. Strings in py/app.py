7f68f65fa1ef6933ea4108997a30949568436a1d
tftprodigy <Olurombisammy@gmail.com>
2024-03-22 02:12:10 +0100

    strings, features and string functions

---
+++ 03. Strings in py/app.py
@@ -0,0 +1,18 @@
+# print ('Hi')
+# print('Hello. \n How are you')
+name = 'ola'
+work = "Developer"
+# print(name.upper())
+# print(name.lower())
+
+# print (name.islower())
+# print (name.isupper())
+
+# let convert a string and join multiple string function toge
+# ther
+# print (name.upper().isupper())
+# print(len(name))
+# print (work)
+# print(len(work))
+
+print (name.index('a'))
