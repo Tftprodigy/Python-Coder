06a8e6f769a0f01a7b70fc52705e3d55f511bc2a
tftprodigy <Olurombisammy@gmail.com>
2024-03-24 21:36:12 +0100

    numbers in py

---
+++ 04. Numbers in Py/app.py
@@ -0,0 +1,31 @@
+from math import *
+
+
+name ="Test"
+
+# print(name.lower())
+# print(name.isupper())
+
+# number = 55
+# number2 = str(number)
+
+# print(number2)
+
+# using absolute number function 
+
+print(abs(-5))
+
+# using max number function 
+# age = 24
+# level = 500
+# urge = 550
+# print (max(age, level, urge))
+
+# # using min number function 
+# print(min(10, 12, 17, 25))
+
+# print(round(3.5))
+
+# print(bin(2))
+
+print (sqrt(25))
