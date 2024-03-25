ea726e4fb6634ed445e7e89b41436fb81d16921c
tftprodigy <Olurombisammy@gmail.com>
2024-03-26 00:16:41 +0100

    List In python

---
+++ 07. List in py/app.py
@@ -0,0 +1,37 @@
+
+from audioop import add
+
+
+countries = ["United Kingdom", "Nigeria", "Ghana", 'USA']
+# print(countries[0])
+# print(countries[1:])
+# print (countries[1:3])
+
+
+# getting the type of the list
+
+# print (type(countries))
+
+# name = "Ola"
+# print(type(name))
+
+# countries[3] = 'SA'
+# print(countries)
+
+# getting lenght of list 
+
+# print (len(countries))
+
+#  a list can display mutiple variabe type 
+# lets mix it up with strings and integers
+
+continents = ["Asia", "Africa", "Europe", 23]
+# print (continents)
+
+# print (continents[3])
+
+# USING LIST CONSTRUCTOR
+# list (()) = list contructor
+
+Dept = list (("CSC", 'math', 25, True))
+print (Dept)
