5548cfdee3bef44e2dd2274088c3e9a7ac3ab263
tftprodigy <Olurombisammy@gmail.com>
2024-03-26 13:46:52 +0100

    list methods in py

---
+++ 08. List Methods/app.py
@@ -0,0 +1,51 @@
+from os import remove
+
+
+list1 = list((1,2,3, 4, 5,10,7, 6))
+list_fruits = ['apples', 'banana', 'mango']
+
+# print (list1)
+# print (list_fruits)
+
+# using extend 
+
+# list1.extend(list_fruits)
+# print(list1)
+
+# using append 
+# list_fruits.append('Orange')
+# print (list_fruits)
+
+# # using insert
+# list_fruits.insert(1, 'Corn')
+# print(list_fruits)
+
+# # using remove
+# list_fruits.remove('apples')
+# print(list_fruits)
+
+# # using clear 
+# list1.clear()
+# print (list1)
+
+# # using index 
+# print(list_fruits.index('mango'))
+
+# # using count 
+# print (list_fruits.count("mango"))
+
+# using sort 
+# list1.sort()
+# print (list1)
+
+# # using reverse 
+# list_fruits.reverse()
+# print(list_fruits)
+
+# using copy 
+# list3 = list1.copy()
+# print (list3)
+
+# using del
+del list1[0]
+print(list1)
