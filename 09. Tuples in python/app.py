33677170842aaf1f1aea28f28ad65bbd8cd7a7fd
tftprodigy <Olurombisammy@gmail.com>
2024-04-05 13:53:38 +0100

    turples in python

---
+++ 09. Tuples in python/app.py
@@ -0,0 +1,9 @@
+three_numbers = (1,2, 3, 4)
+strings = ('home', 'land', 'earth')
+boo = (True, False)
+# print(three_numbers)
+# print (strings)
+
+combined = (1,2,5, 'let', 'me', 'code')
+
+print (combined)
