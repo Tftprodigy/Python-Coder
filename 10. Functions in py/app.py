866b1388aa1a243e15165189bb0554fea43ae659
tftprodigy <Olurombisammy@gmail.com>
2024-04-12 01:22:02 +0100

    functions in py

---
+++ 10. Functions in py/app.py
@@ -0,0 +1,27 @@
+# # Normal function
+# def greetings_function():
+#     print("hello user")
+    
+# greetings_function()
+
+# function using arguments and parameters.
+# from networkx import johnson
+
+# def greetings_function_arg(name, age):
+#     print("Hello", name+ ". you are", age, "years")
+    
+# greetings_function_arg("jOHN", 34)
+
+
+# FUNTION that interact with user 
+from os import name
+
+
+def ask_user_greetings(name, age):
+    name = input('what is your name?')
+    age = input('How old are you?')
+    
+ask_user_greetings(name, age)
+
+    
+
