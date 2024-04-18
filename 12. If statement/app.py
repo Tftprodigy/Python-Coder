6dab0c7d53848b5c089aae836cb708d407d5edca
tftprodigy <Olurombisammy@gmail.com>
2024-04-18 15:54:30 +0100

    if statements

---
+++ 12. If statement/app.py
@@ -0,0 +1,46 @@
+
+
+
+# a =6 
+# b =3
+
+# # if condition:
+#     # Expression
+    
+# if a > b:
+#     print(a , "is greater than ", b)
+    
+# using the same if statement to statement to check
+# strings 
+# name = 'Tim'
+# surname = 'Tim'
+
+# if name == surname:
+#         print("name is equal to surname")
+        
+        
+        
+# using the same if statement to statement to check
+# strings 
+# a = False
+# b= True
+
+# # if a == True:
+# #     print("a is true")
+
+# if a != True:
+#     print("a is not True")
+
+# using if statement with multiple logical operators
+a = 33
+b = 3
+
+# if a >= b:
+#     print("True")
+
+# Adding else block to if statements
+
+if a == b:
+    print ("A equals B")
+else:
+    print('A not equals B')
