6c43a7e372c569f6d2aa1d5768897fc84bbc9cfb
tftprodigy <Olurombisammy@gmail.com>
2024-04-19 15:54:31 +0100

    chapter 14: Dictionaries in py

---
+++ 14. Dictionaries in py/app.py
@@ -0,0 +1,17 @@
+my_dict = {
+    'name': "Ola",
+    'Nationality': "Nigerian",
+    'Qualification': "Bsc",
+    'age': 25,
+    # adding list to the dictionary
+    'friends': ['peter', "john", "Henny"]
+}
+
+print(my_dict['friends'])
+
+
+# -Searching through a dictionary with the key to get 
+    # the value assign to such key 
+# print(my_dict['name'])
+# print(my_dict['Nationality'])
+# print(my_dict['Qualification'])
