935c3921cf829b86f5e93b33ac4f890f6d0c0cad
tftprodigy <Olurombisammy@gmail.com>
2024-04-19 15:18:12 +0100

    chapter 13 project.

---
+++ 13. project (program to calculae if a number is even/app.py
@@ -0,0 +1,15 @@
+
+
+num = int(input('Enter a number: '))
+# checking if num is greater or less than 10
+# if num > 10 :
+#     print(num, "is greater than 10")
+# else :
+#     print(num, 'is less than 10')
+
+# checking is num is even or odd number
+
+if num % 2 == 0:
+    print(num, 'is an Even number')
+else:
+    print(num, "is an odd number")
