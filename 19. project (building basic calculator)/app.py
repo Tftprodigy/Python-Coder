8da612470cd41198559bc4f9e182dfc332672cb8
tftprodigy <Olurombisammy@gmail.com>
2024-04-29 15:27:30 +0100

    chapter 19: Building calculaor in py

---
+++ 19. project (building basic calculator)/app.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter first number: "))
+
+operator = input("Enter Operator ")
+
+num2 = int(input('Enter second number: '))
+
+
+
+if operator == "+": 
+    print(num1 + num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 * num2)
+else:
+    print("wrong inputs or operators")
+    
+
